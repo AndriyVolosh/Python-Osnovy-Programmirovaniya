aefe0b500a0495939adb4f72d3ce92365f50915b
Andriy Volosh <andriy.volosh@gmail.com>
2017-12-02 16:11:00 +0200

    Add files via upload

---
+++ Week1/Следующее и предыдущее.py
@@ -0,0 +1,6 @@
+# Ввод исходного числа
+N = int(input())
+# Вывод первой строки
+print('The next number for the number ', N, ' is ', N+1, '.', sep='')
+# Вывод второй строки
+print('The previous number for the number ', N, ' is ', N-1, '.', sep='')
